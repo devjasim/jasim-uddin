9db78df41a9e06ffcb9328400ba1497683ebbae3
devjasim <jasimfbd@gmail.com>
2024-04-28 11:41:32 +0600

    chore: updated the active menu

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Link from 'next/link';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 import React from 'react';
 
@@ -8,7 +9,21 @@ import TextView from '@components/common/TextView';
 import ThemeToggle from '@components/common/ThemeToggle';
 
 export default function Header() {
+    const [hash, setHash] = React.useState('');
+
+    React.useEffect(() => {
+        setHash(window.location.hash);
+        const handleHashChange = () => {
+            setHash(window.location.hash);
+        };
+        window.addEventListener('hashchange', handleHashChange);
+        return () => {
+            window.removeEventListener('hashchange', handleHashChange);
+        };
+    }, []);
+
     const [isMenuOPen, setIsMenuOpen] = React.useState(false);
+
     return (
         <div className="navbar flex items-center fixed shadow-md top-0 z-50 dark:bg-secondary h-14 md:h-16 lg:h-20 w-full">
             <div className="flex justify-between !px-4 items-center container mx-auto">
@@ -18,22 +33,22 @@ export default function Header() {
                 <div className="navbar-end">
                     <div className={`navbar-items lg:flex ${isMenuOPen ? 'show-menu' : 'hide-menu'}`}>
                         <ul className="menu flex justify-between items-center gap-x-5 xl:gap-x-8 2xl:gap-x-10">
-                            <li className="navbar-item">
+                            <li className={`navbar-item ${hash?.includes('introduction') ? 'active' : ''}`}>
                                 <a href="/#introduction">Home</a>
                             </li>
-                            <li className="navbar-item">
+                            <li className={`navbar-item ${hash?.includes('about') ? 'active' : ''}`}>
                                 <a href="/#about">About</a>
                             </li>
-                            <li className="navbar-item">
+                            <li className={`navbar-item ${hash?.includes('education') ? 'active' : ''}`}>
                                 <a href="/#education">Education</a>
                             </li>
-                            <li className="navbar-item">
+                            <li className={`navbar-item ${hash?.includes('experience') ? 'active' : ''}`}>
                                 <a href="/#experience">Experience</a>
                             </li>
-                            <li className="navbar-item">
+                            <li className={`navbar-item ${hash?.includes('section-projects') ? 'active' : ''}`}>
                                 <a href="/#section-projects">Projects</a>
                             </li>
-                            <li className="navbar-item">
+                            <li className={`navbar-item ${hash?.includes('contact') ? 'active' : ''}`}>
                                 <a href="/#contact">Contact</a>
                             </li>
                             <li>
